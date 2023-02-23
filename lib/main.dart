828e4db2d3a6ad0b5f4dbc94d9b01c4253a2fa29
Sanush CJ <sanushcj@gmail.com>
2023-02-23 16:55:19 +0530

    splash screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'view/splash/splashscreen.dart';
 
 void main() {
--- lib/view/splash/splashscreen.dart
+++ lib/view/splash/splashscreen.dart
@@ -1,20 +1,30 @@
 import 'package:brewapp_task/core/colors/colors.dart';
+import 'package:brewapp_task/view/login/loginscreen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+gotoHome();
+
     double heightof = MediaQuery.of(context).size.height;
     double widthof = MediaQuery.of(context).size.height;
 
-    return Container(
-      color: premiumYellow,
-      height: heightof,
-      width: widthof,
+    return Scaffold(
+      body: Container(
+        color: premiumYellow,
+        height: heightof,
+        width: widthof,
+        child: const Center(child: Text('BrewAPPS TASK',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40),),),
+      ),
     );
+  }
+  gotoHome()async{
+   await Future.delayed(const Duration(seconds: 5));
+  await  Get.to(()=> const LoginScreen());
   }
 }
