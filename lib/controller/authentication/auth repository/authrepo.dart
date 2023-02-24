c1fc2294f3a8d5a56f2126fbaaf51f12961bb54e
Sanush CJ <sanushcj@gmail.com>
2023-02-24 14:16:16 +0530

    functions adding

--- lib/controller/authentication/auth repository/authrepo.dart
+++ lib/controller/authentication/auth repository/authrepo.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class AuthenticationRepo extends GetxController {
   static AuthenticationRepo get instance => Get.find();
 
-
+final _auth = FirebaseAuth.instance;
 
 
 
